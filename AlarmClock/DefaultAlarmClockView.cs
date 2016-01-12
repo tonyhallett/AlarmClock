de34cd9ab7295bc4cd4b41349d971bda340ba25e
Tony Hallett <tonyhallett74@gmail.com>
2016-01-12 15:22:01 +0000

    IAlarmClockView default implementation

---
+++ AlarmClock/DefaultAlarmClockView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlarmClock
+{
+    public partial class DefaultAlarmClockView : Form, IAlarmClockView
+    {
+        public DefaultAlarmClockView()
+        {
+            InitializeComponent();
+            originalAlarmColor = this.BackColor;
+        }
+
+        public event EventHandler TurnOffAlarm;
+        private Color originalAlarmColor;
+        public void StartAlarm()
+        {
+            this.BackColor = Color.Red;
+        }
+
+        public void StopAlarm()
+        {
+            this.BackColor = originalAlarmColor;
+        }
+    }
+}
