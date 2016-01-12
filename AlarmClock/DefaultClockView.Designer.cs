8c008681dd147f5b24f16f60f575e513a5e4fd06
Tony Hallett <tonyhallett74@gmail.com>
2016-01-12 15:35:37 +0000

    Default IClockView implementation

---
+++ AlarmClock/DefaultClockView.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace AlarmClock
+{
+    partial class DefaultClockView
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(20, 18);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(36, 13);
+            this.labelTime.TabIndex = 0;
+            this.labelTime.Text = "Time: ";
+            // 
+            // DefaultClockView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.labelTime);
+            this.Name = "DefaultClockView";
+            this.Size = new System.Drawing.Size(150, 49);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTime;
+    }
+}
---
+++ AlarmClock/DefaultClockView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlarmClock
+{
+    public partial class DefaultClockView : UserControl,IClockView
+    {
+        public DefaultClockView()
+        {
+            InitializeComponent();
+        }
+
+        public DateTime Time
+        {
+            set
+            {
+                labelTime.Text = value.ToShortDateString();
+            }
+        }
+    }
+}
